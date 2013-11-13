ba961bfae2858db1e80f09076a2832b69593ca0a
ivanbrennan <ivan.brennan@gmail.com>
2013-11-13 13:22:49 -0500

    student progress so far

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,17 @@ class Song < ActiveRecord::Base
   def artist_name=(artist_name)
     self.artist = Artist.find_or_create_by(:name => artist_name)
   end
+
+  def genre_names=(csv_genre_names)
+    self.add_genre(Genre.find_or_create_by_csv_string(csv_genre_names))
+#    genre_names = csv_genre_names.split(",").collect{|g| g.strip.downcase}
+#    genre_names.each do |genre_name|
+#      genre = Genre.find_or_create_by(:name => genre_name)
+#      self.add_genre(genre)
+#    end
+  end
+
+  def add_genre(genre)
+    self.song_genres.build(:genre => genre)
+  end
 end
